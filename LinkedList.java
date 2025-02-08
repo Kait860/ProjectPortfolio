7f8849ea0d43ca33843d9c94156fff9b0abbde7f
Kait860 <schneiderkaitlyn04@gmail.com>
2025-02-07 19:56:58 -0600

    Delete LinkedList.java

--- LinkedList.java
+++
@@ -1,8 +0,0 @@
-public class LinkedList{
-    public static void main(String[] args) throws Exception {
-        LinkedList<VehicleData> vehicleList = new LinkedList<VehicleData>();
-
-        
-    }
-
-}
