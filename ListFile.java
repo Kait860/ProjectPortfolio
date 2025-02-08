f9911b402e2ee26dfc19fb6ae629a73cf0ee0908
Kait860 <schneiderkaitlyn04@gmail.com>
2025-02-07 19:56:20 -0600

    Add files via upload

--- ListFile.java
+++ ListFile.java
@@ -1,8 +1,36 @@
 import java.util.LinkedList;
+import java.util.Scanner;
+
 public class ListFile{
     public static void main(String[] args) throws Exception {
+        //creates a linked list of type vehicle
         LinkedList<Vehicle> vehicleList = new LinkedList<Vehicle>();
+        //initializes scanner and variables
+        Scanner scnr = new Scanner(System.in);
+        String make;
+        String model;
+        Double mpg;
 
+        //loop that continues until there is no input left
+        while(scnr.hasNext() == true){
+            try{
+                //scans values into VehicleList linked list
+                make = scnr.next();
+                model = scnr.next();
+                mpg = scnr.nextDouble();
+                vehicleList.add(new Vehicle(make, model, mpg));
+                //creates message for invalid mpg input
+                if(mpg < 6 || mpg > 90){
+                    throw new IllegalArgumentException("That is not a valid miles per gallon input!");
+                }
+            //catches if the mpg input is between 6 - 90 as there shouldn't be an mpg outside of that range
+            }catch(IllegalArgumentException e){
+                //prints catch message
+                System.out.println(e.getMessage());
+            }
+        }
+        //closes scanner
+        scnr.close();
         
     }
 
--- Vehicle.java
+++ Vehicle.java
@@ -3,25 +3,15 @@ public class Vehicle {
     private String model;
     private Double mpg;
 
+    //method used to create new Vehicle object
     public Vehicle(String make, String model, Double mpg){
         this.make = make;
         this.model = model;
         this.mpg = mpg;
     }
 
-    public String getMake(){
-        return make;
-    }
-
-    public String getModel(){
-        return model;
-    }
-
-    public Double getMpg(){
-        return mpg;
-    }
-
+    //the method I am going to use to easily print the information to the file
     public String toString(){
-        return "Make: " + make + " Model: " + model + " Miles Per Gallon: " + mpg;
+        return "Make: " + make + " Model: " + model + " Miles Per Gallon: " + mpg + "\n";
     }
 }
